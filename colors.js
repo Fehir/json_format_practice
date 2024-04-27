143e695afa07c2157f9884d54f5220b7ce5890f3
ssm.fehir <fehir.1996@gmail.com>
2024-04-27 06:32:12 +0600

    colors & maps

---
+++ colors.js
@@ -0,0 +1,62 @@
+let listOfColor;
+listOfColor = {
+    "colors": [
+        {
+            "color": "black",
+            "category": "hue",
+            "type": "primary",
+            "code": {
+                "rgba": [255, 255, 255, 1],
+                "hex": "#000"
+            }
+        },
+        {
+            "color": "white",
+            "category": "value",
+            "code": {
+                "rgba": [0, 0, 0, 1],
+                "hex": "#FFF"
+            }
+        },
+        {
+            "color": "red",
+            "category": "hue",
+            "type": "primary",
+            "code": {
+                "rgba": [255, 0, 0, 1],
+                "hex": "#FF0"
+            }
+        },
+        {
+            "color": "blue",
+            "category": "hue",
+            "type": "primary",
+            "code": {
+                "rgba": [0, 0, 255, 1],
+                "hex": "#00F"
+            }
+        },
+        {
+            "color": "yellow",
+            "category": "hue",
+            "type": "primary",
+            "code": {
+                "rgba": [255, 255, 0, 1],
+                "hex": "#FF0"
+            }
+        },
+        {
+            "color": "green",
+            "category": "hue",
+            "type": "secondary",
+            "code": {
+                "rgba": [0, 255, 0, 1],
+                "hex": "#0F0"
+            }
+        },
+    ]
+}
+
+console.log(listOfColor.colors[5].code.rgba[3]);
+console.log(listOfColor.colors[5].color);
+console.log(listOfColor.colors[2].category);
---
+++ googlemaps.js
@@ -0,0 +1,19 @@
+let googleMaps = {
+    "markers": [
+      {
+        "name": "Rixos The Palm Dubai",
+        "position": [25.1212, 55.1535],
+      },
+      {
+        "name": "Shangri-La Hotel",
+        "location": [25.2084, 55.2719]
+      },
+      {
+        "name": "Grand Hyatt",
+        "location": [25.2285, 55.3273]
+      }
+    ]
+  }
+  
+  console.log(googleMaps.markers[0].position);
+  console.log(googleMaps.markers[2].name);
